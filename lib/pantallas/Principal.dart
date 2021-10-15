1a2fc606f12788e6fe33cdc4f1c616cffa4073b1
UTEC-MauricioNovo <mauricio.novo@estudiantes.utec.edu.uy>
2021-10-14 21:55:47 -0300

    logo app

--- lib/pantallas/Principal.dart
+++ lib/pantallas/Principal.dart
@@ -69,7 +69,7 @@ class Principal extends StatelessWidget {
         children: [
           ListTile(
               subtitle: Text(
-                  'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.',
+                  'Única plataforma Uruguaya para intérpretes de Lengua de Señas donde encontrarás, con un solo clic, la información que requieras para tu cotidianidad',
                   textAlign: TextAlign.justify)),
         ],
       ),
